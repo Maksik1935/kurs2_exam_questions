32ad19bd9266c6cc430818abe947e574029cd512
m-kac <m-kachalov@mail.ru>
2022-07-24 17:00:45 +0300

    Initial commit

---
+++ src/main/java/sky/pro/kurs2_exam_questions/Kurs2ExamQuestionsApplication.java
@@ -0,0 +1,13 @@
+package sky.pro.kurs2_exam_questions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kurs2ExamQuestionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kurs2ExamQuestionsApplication.class, args);
+    }
+
+}
